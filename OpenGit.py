95e11ad65aea8bc9f2f23d383052808b25008c38
yourilima <yourilima@gmail.com>
2013-12-23 00:39:25 +0100

    Update OpenGit.py

--- OpenGit.py
+++ OpenGit.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin
+import webbrowser
 
-class OpenGitCommand(sublime_plugin.TextCommand):
+class OpenGitCommand(sublime_plugin.WindowCommand):
     def run(self,edit):
-       return
+        view = sublime.active_window().active_view()
+        file = view.file_name()
+        WindowCommand.open_new_tab('https://gitlab.prx.dk/%()s' % file)
